7d13e1f2198675127d6e237c16f2c502aae74191
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-07-12 23:34:49 -0400

    Add sketch capabilities to grid
    Add sketch capabilities such that when a user hovers over
    a grid box, it gets filled in with black.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const grid = document.querySelector(".grid");
 
+// function to add background color
+function changeBackgroundColor(e) {
+  e.target.style.backgroundColor = "black";
+}
+
 // add grid of 16x16 square divs
 for (let i = 0; i < 16; i++) {
   let row = document.createElement("div");
@@ -8,6 +13,7 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let gridBox = document.createElement("div");
     gridBox.classList.add("grid-box");
+    gridBox.addEventListener("mouseover", changeBackgroundColor);
     row.appendChild(gridBox);
   }
 }
