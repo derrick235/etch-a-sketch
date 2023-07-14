374e50e7c08a97f7273c32dc21ad37e5e497f07f
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-07-13 23:00:41 -0400

    Add resizing functionality
    Add resize button that changes size of grid while preserving
    the 480x480px dimensions. User is stopped if a value greater than
    100 is entered.

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ for (let i = 0; i < 16; i++) {
     row.appendChild(gridBox);
   }
 }
+
+// add button functionality
+
+function changeSize(e) {
+  let newSize = prompt("Enter a new size:");
+  if (newSize > 100) {
+    alert("Only values 100 and less are accepted.");
+    return;
+  }
+
+  // remove current grid
+  let rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    grid.removeChild(row);
+  })
+
+  // add new grid
+  for (let i = 0; i < newSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    grid.appendChild(row);
+    for (let j = 0; j < newSize; j++) {
+      let gridBox = document.createElement("div");
+      gridBox.classList.add("grid-box");
+      gridBox.addEventListener("mouseover", changeBackgroundColor);
+      row.appendChild(gridBox);
+    }
+  }
+
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", changeSize);
